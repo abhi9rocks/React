cce2a56c604dc3929dd70869eea3ab3105aa866a
Abhinav Kumar Singh <aks.singh774@gmail.com>
2022-11-28 19:10:59 +0530

    upload

--- src/index.js
+++ src/index.js
@@ -4,22 +4,32 @@ import ReactDom from 'react-dom';
 //CSS
 import './index.css';
 
+const firstBook={
+author:'Abhinav Rules',
+title :'Atomic Habits: The life-changing million',
+img :"https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg"
+}
+
 function BookList() {
   return (
-    <section>
-      <Book/>
+    <section className='booklist'>
+      <Book img={firstBook.img} title={firstBook.title}/>
+      <Book title='random title' number={22}/>
+      
     </section>
-  );
-  
+  ); 
 }
-const Book = () =>{
-  return (<article><Image></Image>
-          <Title/>
+
+const Book = (props) =>{
+  
+  return (<article className="book">
+          <img src={props.img} alt=""/>
+          <h1>{title}</h1>
+          <h4>{author.toUpperCase()}</h4>
+          <p>{props.job}</p>
+          <p>{props.title}</p>
           </article>
   );
 };
-const Image = () => (<img src="https://images-eu.ssl-images-amazon.com/images/I/91bYsX41DVL._AC_UL600_SR600,400_.jpg" alt=""/>
-);
 
-const Title = () => <h1>Atomic Habits: The life-changing million</h1>
 ReactDom.render(<BookList/>, document.getElementById('root'));
